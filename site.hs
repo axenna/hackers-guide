9caf29e34d077026dd45f48684fcbd0866563489
Zenna Allwein <zrallwein@gmail.com>
2023-09-26 08:12:27 -0500

    initial commit

---
+++ site.hs
@@ -0,0 +1,42 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid (mappend)
+import           Hakyll
+
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    match "posts/*" $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler
+
+    match "index.html" $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let indexCtx =
+                    listField "posts" previewCtx (return posts) <>
+                    defaultContext
+
+            getResourceBody
+                >>= applyAsTemplate indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= relativizeUrls
+
+    match "templates/*" $ compile templateBodyCompiler
+
+
+--------------------------------------------------------------------------------
+previewCtx :: Context String
+previewCtx =
+    (mapContext (take 200) (bodyField "preview")) <>
+    defaultContext
